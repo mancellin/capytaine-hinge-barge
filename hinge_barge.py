6432de47182c4fa390e3da7547b94ed64e2393cb
Matthieu Ancellin <matthieu.ancellin@ens-paris-saclay.fr>
2020-03-21 17:39:47 +0100

    Clean up code.

--- hinge_barge.py
+++ hinge_barge.py
@@ -2,7 +2,6 @@
 # coding: utf-8
 
 from collections import namedtuple
-from typing import Sequence
 
 import numpy as np
 import xarray as xr
@@ -12,16 +11,14 @@ from scipy.linalg import block_diag
 import capytaine as cpt
 from meshmagick.hydrostatics import Hydrostatics
 
-
-FOUR_DOFS_NAMES = ['Heave', 'Pitch', 'body_1__Relative_Pitch', 'body_3__Relative_Pitch']
-RHO_WATER = 1025.0
-
 RectangleParams = namedtuple("RectangleParams", ["name", "shape", "geometric_center", "center_of_mass", "resolution"])
 
 DEFAULT_BODY_1 = RectangleParams(name="body_1", shape=(0.68, 0.4, 0.10), geometric_center=(0.0, 0.0, 0.0), center_of_mass=(0.0, 0.0, -0.025), resolution=10)
 DEFAULT_BODY_2 = RectangleParams(name="body_2", shape=(0.28, 0.4, 0.15), geometric_center=(0.0, 0.0, 0.0), center_of_mass=(0.0, 0.0, -0.025), resolution=10)
 DEFAULT_BODY_3 = RectangleParams(name="body_3", shape=(1.00, 0.4, 0.10), geometric_center=(0.0, 0.0, 0.0), center_of_mass=(0.0, 0.0, -0.025), resolution=10)
 DEFAULT_DAMPING_PLATE = RectangleParams(name="damping_plate", shape=(0.48, 0.4, 0.01), geometric_center=(0.0, 0.0, -0.227), center_of_mass=(0.0, 0.0, -0.227), resolution=10)
+DEFAULT_BARGE = ((DEFAULT_BODY_1,), (DEFAULT_BODY_2, DEFAULT_DAMPING_PLATE), (DEFAULT_BODY_3,))
+DEFAULT_DOFS = ['Heave', 'Pitch', 'body_1__Relative_Pitch', 'body_3__Relative_Pitch']
 
 
 class HingeBarge(cpt.FloatingBody):
@@ -30,14 +27,17 @@ class HingeBarge(cpt.FloatingBody):
     Each body can be composed of several rectangular parallelepiped moving together.
     """
 
-    def __init__(
-            self,
-            bodies=((DEFAULT_BODY_1,), (DEFAULT_BODY_2, DEFAULT_DAMPING_PLATE), (DEFAULT_BODY_3,)),
-            center=(0.88, 0, 0),
-            distance_between_bodies=0.06, clever_mesh=False,
-            dofs_names=FOUR_DOFS_NAMES, pto=None,
-    ):
+    ##################
+    #  MESH AND DOF  #
+    ##################
+
+    def __init__(self, *,
+                 bodies=DEFAULT_BARGE,
+                 dofs_names=DEFAULT_DOFS,
+                 distance_between_bodies=0.06,
+                 center=(0.0, 0.0, 0.0)):
         """Initialize the mesh and properties of a hinge-barge."""
+
         self.bodies_params = bodies
         self.center = np.array(center)
         self.distance_between_bodies = distance_between_bodies
@@ -62,11 +62,6 @@ class HingeBarge(cpt.FloatingBody):
         self.translate(center)
         self.keep_immersed_part()
 
-        # Compute static properties
-        self._compute_mass()
-        self._compute_hydrostatic_stiffness()
-        self._compute_pto(pto)
-
     @staticmethod
     def _generate_mesh_of_a_parallelepiped(params: RectangleParams):
         """Generate the mesh of a single parallelepiped using capytaine mesh generator."""
@@ -78,7 +73,6 @@ class HingeBarge(cpt.FloatingBody):
             size=params.shape,
             resolution=resolution,
             center=params.geometric_center,
-            # reflection_symmetry=params.clever_mesh,
             name=params.name,
         )
         body.center_of_mass = np.asarray(params.center_of_mass)
@@ -89,7 +83,6 @@ class HingeBarge(cpt.FloatingBody):
     def _generate_meshes(self):
         """Create the mesh of the bodies one after the other."""
         self.bodies = []
-        self.individual_hydrostatic_stiffness = []
         x_position_of_next_body = 0.0
 
         for body_params in self.bodies_params:
@@ -109,9 +102,6 @@ class HingeBarge(cpt.FloatingBody):
             x_position_of_next_body += body_params[0].shape[0] + self.distance_between_bodies
 
             self.bodies.append(body)
-            self.individual_hydrostatic_stiffness.append(
-                Hydrostatics(body.mesh.merged().to_meshmagick()).hs_data['stiffness_matrix']
-            )
 
     def _global_dof_from_individual_dofs(self, dof_name):
         """All the dofs of the hinge-barge can be seen as linear combination
@@ -166,7 +156,11 @@ class HingeBarge(cpt.FloatingBody):
         """Matrix of the coefficients discussed in the function above."""
         return np.array([self._global_dof_from_individual_dofs(dof) for dof in self.dofs])
 
-    def _compute_mass(self):
+    ####################################
+    #  HYDROSTATICS AND HYDRODYNAMICS  #
+    ####################################
+
+    def _compute_mass(self, rho_water):
         """Compute the mass matrix for the hinge-barge by combining the mass matrices
         of the individual parallelepiped composing the full hinge-barge."""
 
@@ -177,7 +171,7 @@ class HingeBarge(cpt.FloatingBody):
             """
             # TODO: Test for all center_of_mass and all dofs.
             draught = params.shape[2]/2
-            mass = RHO_WATER * params.shape[0] * params.shape[1] * draught
+            mass = rho_water * params.shape[0] * params.shape[1] * draught
             return np.diag([
                 mass, mass, mass,
                 (mass * (params.shape[1]**2 + params.shape[2]**2))/12 + mass*(params.center_of_mass[2]**2), # Roll
@@ -188,14 +182,43 @@ class HingeBarge(cpt.FloatingBody):
         P = self._transformation_matrix()
         self.mass = self.add_dofs_labels_to_matrix(P @ block_diag(*blocks) @ P.T)
 
-    def _compute_hydrostatic_stiffness(self):
+    def _compute_hydrostatic_stiffness(self, rho_water=1025.0):
         """Compute the hydrostatic stiffness for the hinge-barge by combining the hydrostatic stiffnesses
         of the individual parallelepiped composing the full hinge-barge."""
+
+        self.individual_hydrostatic_stiffness = []
+        for body in self.bodies:
+            self.individual_hydrostatic_stiffness.append(
+                Hydrostatics(body.mesh.merged().to_meshmagick(), rho_water=rho_water).hs_data['stiffness_matrix']
+            )
+
         individual_hs = [np.zeros((6, 6)) for _ in range(len(self.bodies))]
         for i in range(len(self.bodies)):
             individual_hs[i][2:5, 2:5] = self.individual_hydrostatic_stiffness[i]
         P = self._transformation_matrix()
         self.hydrostatic_stiffness = self.add_dofs_labels_to_matrix(P @ block_diag(*individual_hs) @ P.T)
+
+    def compute_hydrostatics(self, rho_water=1025.0):
+        self._compute_mass(rho_water)
+        self._compute_hydrostatic_stiffness()
+
+    def compute_hydrodynamics(self,
+                              omega_range=np.linspace(2, 10, 40),
+                              wave_direction_range=[np.pi],
+                              rho_water=1025.0,
+                              ):
+        dataset = xr.Dataset(coords={
+            'omega': omega_range,
+            'wave_direction': wave_direction_range,
+            'radiating_dof': list(self.dofs),
+            'rho': rho_water,
+        })
+        self.dataset = cpt.Nemoh(linear_solver="gmres").fill_dataset(dataset, [self])
+        return self.dataset
+
+    ####################
+    # MOTION AND POWER #
+    ####################
 
     def _compute_pto(self, pto=None):
         if pto is None:
@@ -210,22 +233,10 @@ class HingeBarge(cpt.FloatingBody):
             np.diag([self.pto.sel(radiating_dof=name) for name in self.dofs.keys()])
         )
 
-
-    def compute_hydrodynamics(self,
-                              omega_range=np.linspace(2, 10, 40),
-                              wave_direction_range=[np.pi],
-                              ):
-        dataset = xr.Dataset(coords={
-            'omega': omega_range,
-            'wave_direction': wave_direction_range,
-            'radiating_dof': list(self.dofs),
-            'rho': RHO_WATER,
-        })
-        self.dataset = cpt.Nemoh(linear_solver="gmres").fill_dataset(dataset, [self])
+    def compute_motion(self, wave_amplitude=0.02, viscous_dissipation=None, pto=None):
+        self._compute_pto(pto)
         self.dataset['PTO_matrix'] = self.Kpto
-        return self.dataset
 
-    def compute_motion(self, wave_amplitude=0.02, viscous_dissipation=None):
         if viscous_dissipation is None:
             viscous_dissipation = np.zeros(self.Kpto.shape)
 
@@ -242,21 +253,4 @@ class HingeBarge(cpt.FloatingBody):
         self.dataset['power'] = power_per_dof.sum(dim="influenced_dof")
         return self.dataset['power']
 
-
-        # if plot_style is None:
-        #     plot_style = {}
-        # self.dataset.attrs['plot_style'] = plot_style
-
-
-if __name__ == "__main__":
-    import logging
-    logging.basicConfig(level=logging.INFO)
-
-    hb = HingeBarge(dofs_names=FOUR_DOFS_NAMES + ["Surge"])
-    # print(hb.mass)
-    # print(hb.hydrostatic_stiffness)
-    # print(hb.Kpto)
-    hb.compute_hydrodynamics()
-    print(hb.dataset)
-    # print(hb.animate_dof("body_1__relative_pitch", 1.0))
 
